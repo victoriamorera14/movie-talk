545b75295163fee3dd5c5c4c930c697631308c8f
miquelmontes <105371879+miquelmontes@users.noreply.github.com>
2024-04-29 18:09:12 +0200

    mejora visual de app.js

--- App.js
+++ App.js
@@ -15,38 +15,54 @@ function ShowComponent({ name, children }) {
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <ShowComponent name="Avatar">
-        <Avatar name="Paula Rodriguez" />
-        <Avatar name="Joan Roca" color="#B255DE" />
-        <Avatar image={require("./assets/user.jpg")} />
-      </ShowComponent>
-      <ShowComponent name="Mark">
-        <Mark unread={true} />
-        <Mark unread={false} />
-        <Mark double={true} />
-        <Mark unread={true} size={32} />
-        <Mark unread={false} size={32} />
-        <Mark double={true} size={32} />
-      </ShowComponent>
-      <ShowComponent name="Search">
-        <SearchBox>
+    <View style={styles.allComponents}>
+      <Text style={styles.componentsTitle}><h2>Aqui estan tots els components:</h2></Text>
+      <View style={styles.container}>
+        <ShowComponent name="Avatar">
+          <Avatar name="Paula Rodriguez" />
+          <Avatar name="Joan Roca" color="#B255DE" />
+          <Avatar image={require("./assets/user.jpg")} />
+        </ShowComponent>
+        <ShowComponent name="Mark">
+          <Mark unread={true} />
+          <Mark unread={false} />
+          <Mark double={true} />
+          <Mark unread={true} size={32} />
+          <Mark unread={false} size={32} />
+          <Mark double={true} size={32} />
+        </ShowComponent>
+        <ShowComponent name="Search">
+          <SearchBox>
 
-        </SearchBox>
-      </ShowComponent>
+          </SearchBox>
+        </ShowComponent>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  allComponents:{
+    maxWidth: '3500px',
+    border: '2px solid black',
+    alignContent: "center",
+  },
   container: {
     paddingTop: Constants.statusBarHeight,
     flex: 1,
     backgroundColor: "#f1f1f1",
     gap: 10,
+    padding: 100,
   },
+
   showContainer: {},
+
+  componentsTitle:{
+    textAlign: "center",
+  },
+
   showTitle: {},
+
   showRow: {
     flexDirection: "row",
     backgroundColor: "white",
