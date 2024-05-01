c1cc08b0f520f15d01ab487e6b7e2a3244e94d21
miquelmontes <105371879+miquelmontes@users.noreply.github.com>
2024-05-01 19:57:15 +0200

    titulo update

--- components/Titulo.js
+++ components/Titulo.js
@@ -7,7 +7,7 @@ export default function Titulo({ text, showIcon }) {
       {showIcon && <Image source={require("../assets/backIcon.png")} style={styles.icon} />}
       <View style={[styles.textContainer, showIcon && { marginLeft: 10 }]}>
         <Text style={styles.titleText}>{text}</Text>
-        <View style={styles.line}></View>
+        {/*<View style={styles.line}></View>*/}
       </View>
     </View>
   );
@@ -18,27 +18,34 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     alignItems: "center",
     padding: 10,
-    margin:10
+    margin: 10,
+    width: "auto",
+    
   },
   textContainer: {
     flex: 1,
     alignContent: "left",
-    justifyContent: "center" //No consigo que el texto se centre con la linea
+    justifyContent: "center", //No consigo que el texto se centre con la linea
+    borderBottomColor: 'white',
+    borderBottomWidth: 2,
   },
   titleText: {
     color: "#E92348",
     fontSize: 20,
     fontWeight: "bold",
   },
-
+/*
   line: {
     position: "absolute",
     top: "30px",
-    width:280,
+    paddingLeft:5,
+    paddingRight:5,
+    width:"auto",
     borderBottomWidth: 2,
     borderBottomColor: "white",
     borderRadius: 5, 
   },
+  */
   icon: {
     height: "20px",
     padding: 10,
