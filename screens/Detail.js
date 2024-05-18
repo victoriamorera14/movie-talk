75074bc37631435e00bb44a75513a2ab07c6f625
miquelmontes <105371879+miquelmontes@users.noreply.github.com>
2024-05-18 19:03:54 +0200

    details bonito

--- screens/Detail.js
+++ screens/Detail.js
@@ -1,5 +1,13 @@
-import { View, Text, Image, StyleSheet, Pressable, ScrollView } from "react-native";
-import React, { useEffect } from "react";
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  Pressable,
+  ScrollView,
+  Button,
+} from "react-native";
+import React, { useEffect, useState } from "react";
 import API_KEY from "../api/API_KEY";
 import useFetch from "../hooks/useFetch";
 import { colors } from "../utils/colors";
@@ -12,23 +20,25 @@ export default function Detail({ route }) {
   const imagePath = "http://image.tmdb.org/t/p/w500/";
   const { isLoading, error, movies, ApiCall } = useFetch();
 
+  const [showMore, setShowMore] = useState(false);
+
   useEffect(() => {
     ApiCall(DETAILS_API_URL);
   }, []);
 
-  console.log(DETAILS_API_URL)
+  console.log(DETAILS_API_URL);
 
   return (
     <ScrollView style={styles.container}>
       {movies && (
         <>
-        <View style={styles.detailsHeader}>
-          <Image
-            style={styles.image}
-            source={{ uri: `${imagePath}${movies.backdrop_path}` }}
-          />
-          <Text style={styles.title}>{movies.title}</Text>
-        </View>
+          <View style={styles.detailsHeader}>
+            <Image
+              style={styles.image}
+              source={{ uri: `${imagePath}${movies.backdrop_path}` }}
+            />
+            <Text style={styles.title}>{movies.title}</Text>
+          </View>
           <View style={styles.genresContainer}>
             {movies.genres.map((genre) => (
               <Text key={genre.id} style={styles.genre}>
@@ -40,7 +50,14 @@ export default function Detail({ route }) {
             <Text style={styles.voteText}>USER RATING</Text>
             <Text style={styles.vote}>{movies.vote_average}</Text>
           </View>
-          <Text style={styles.overview} numberOfLines={2}>{movies.overview}</Text>
+          <View style={styles.descriptionCobtainer}>
+          <Text style={styles.descriptionTitle}>Description:</Text>
+          <Text style={styles.overview} numberOfLines={showMore ? 15:2}>
+            {movies.overview}
+            
+          </Text>
+          <Pressable onPress={() => setShowMore(!showMore)}><Text style={styles.showMore}>{showMore ? "Show less...":"Show more..."}</Text></Pressable>
+          </View>
           <Text style={styles.platforms}>Available on:</Text>
           {/* {movies.production_companies.map((company) => (
             <Text key={company.id} style={styles.company}>
@@ -56,11 +73,9 @@ export default function Detail({ route }) {
 const styles = StyleSheet.create({
   container: {
     //padding: 20,
-    backgroundColor:colors.mainColors.primary,
+    backgroundColor: colors.mainColors.primary,
   },
-  detailsHeader:{
-    
-  },
+  detailsHeader: {},
   image: {
     height: 300,
     //aspectRatio: 1,
@@ -85,40 +100,57 @@ const styles = StyleSheet.create({
     marginBottom: 5,
     textAlign: "center",
   },
-  genresContainer:{
-    flexDirection:"row",
-    justifyContent:"center",
+  genresContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
     gap: 10,
-    marginBottom: 15,
+    marginBottom: 20,
   },
 
-  userRatingContainer:{
-    flexDirection:"column",
-    justifyContent:"center",
+  userRatingContainer: {
+    flexDirection: "column",
+    justifyContent: "center",
+    marginBottom: 20,
   },
-  voteText:{
-    textAlign:"center",
+  voteText: {
+    textAlign: "center",
     fontSize: 14,
     fontWeight: "bold",
-    color:colors.mainColors.secondary,
-
+    color: colors.mainColors.secondary,
   },
   vote: {
     fontSize: 18,
     textAlign: "center",
     fontWeight: "bold",
-    color:"white",
+    color: "white",
     marginBottom: 10,
   },
-  
-  overview: {
-    fontSize: 16,
-    textAlign: "justify",
-    marginBottom: 10,
+
+  descriptionCobtainer:{
+    marginBottom: 20,
     paddingLeft: 25,
     paddingRight: 25,
   },
-  
+  descriptionTitle:{
+    color:colors.mainColors.secondary,
+    fontWeight: "bold",
+    fontSize: 18,
+    marginBottom: 5,
+  },
+
+  overview: {
+    color:"white",
+    fontSize: 14,
+    textAlign: "justify",
+    marginBottom: 8,
+  },
+
+  showMore:{
+    color:colors.mainColors.secondary,
+    fontSize: 14,
+    textAlign: "justify",
+  },
+
   platforms: {
     fontSize: 18,
     textAlign: "center",
@@ -130,5 +162,4 @@ const styles = StyleSheet.create({
     textAlign: "center",
     marginBottom: 5,
   },
- 
 });
