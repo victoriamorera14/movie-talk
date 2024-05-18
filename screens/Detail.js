a751b2caf7f3cfc205fe8086f6b22bfb49d0cd83
arnaufs <arnaufontsalcedo@gmail.com>
2024-05-18 16:56:25 +0200

    Ya se ven los detalles

--- screens/Detail.js
+++ screens/Detail.js
@@ -1,4 +1,4 @@
-import { View, Text, Image, StyleSheet, Pressable } from "react-native";
+import { View, Text, Image, StyleSheet, Pressable, ScrollView } from "react-native";
 import React, { useEffect } from "react";
 import API_KEY from "../api/API_KEY";
 import useFetch from "../hooks/useFetch";
@@ -15,15 +15,35 @@ export default function Detail({ route }) {
     ApiCall(DETAILS_API_URL);
   }, []);
 
+  console.log(DETAILS_API_URL)
+
   return (
-    <View style={styles.container}>
+    <ScrollView style={styles.container}>
       {movies && (
-        <Image
-          style={styles.image}
-          source={`${imagePath}${movies.backdrop_path}`}
-        />
+        <>
+          <Image
+            style={styles.image}
+            source={{ uri: `${imagePath}${movies.backdrop_path}` }}
+          />
+          <Text style={styles.title}>{movies.title}</Text>
+          <View style={styles.genresContainer}>
+            {movies.genres.map((genre) => (
+              <Text key={genre.id} style={styles.genre}>
+                {genre.name}
+              </Text>
+            ))}
+          </View>
+          <Text style={styles.overview}>{movies.overview}</Text>
+          <Text style={styles.vote}>Rating: {movies.vote_average}</Text>
+          <Text style={styles.platforms}>Available on:</Text>
+          {/* {movies.production_companies.map((company) => (
+            <Text key={company.id} style={styles.company}>
+              {company.name}
+            </Text>
+          ))} */}
+        </>
       )}
-    </View>
+    </ScrollView>
   );
 }
 
@@ -34,11 +54,40 @@ const styles = StyleSheet.create({
   image: {
     width: "100%",
     aspectRatio: 1,
+    marginBottom: 20,
   },
   title: {
+    fontSize: 24,
+    textAlign: "center",
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  overview: {
+    fontSize: 16,
+    textAlign: "justify",
+    marginBottom: 10,
+  },
+  vote: {
     fontSize: 18,
     textAlign: "center",
     fontWeight: "bold",
-    padding: 10,
+    marginBottom: 10,
+  },
+  platforms: {
+    fontSize: 18,
+    textAlign: "center",
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  company: {
+    fontSize: 16,
+    textAlign: "center",
+    marginBottom: 5,
+  },
+  genre: {
+    fontSize: 16,
+    marginHorizontal: 5,
+    marginBottom: 5,
+    textAlign: "center",
   },
 });
