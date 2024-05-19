ba8b8beb9e3f0bb0b1f8cdbcafcd4f1fb90d5d15
miquelmontes <105371879+miquelmontes@users.noreply.github.com>
2024-05-19 22:44:24 +0200

    styles

--- screens/Favorites.js
+++ screens/Favorites.js
@@ -23,6 +23,7 @@ export default observer(function Favorites() {
 
 const styles = StyleSheet.create({
   container: {
+    height: "100%",
     backgroundColor:colors.mainColors.primary,
     padding: 10
   },
