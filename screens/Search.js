e0a1aba92e2e95f7f40db169136e7708a5f6ed81
victoriamorera14 <victoria.morera@estudiantat.upc.edu>
2024-06-01 13:00:33 +0200

    No funcionaba el searcher y ahora si que va

--- screens/Search.js
+++ screens/Search.js
@@ -1,10 +1,9 @@
-import { View, Text, StyleSheet, Pressable, ScrollView, Dimensions} from "react-native";
+import { View, Text, StyleSheet, Pressable, FlatList, Dimensions } from "react-native";
 import React, { useEffect, useState } from "react";
 import MovieCard from "../components/MovieCard";
 import SearchFilter from "../components/SearchFilter";
 import CustomButton from "../components/CustomButton";
 import API_KEY from "../api/API_KEY";
-import { FlatList } from "react-native";
 import useFetch from "../hooks/useFetch";
 import { useNavigation } from "@react-navigation/native";
 import { colors } from "../utils/colors";
@@ -51,96 +50,92 @@ export default function Search({ route }) {
 
   return (
     <>
-      <ScrollView>
-        <View style={styles.backgroundColor}>
-          <View style={styles.topContainer}>
-            <SearchFilter
-              searchMovies={searchMovies}
-              showIconLeft={true}
-              showIconRight={false}
-            />
-            <CustomButton
-              borderRadius={90}
-              defaultColor={"#E92348"}
-              pressedColor={"#373246"}
-              height={38}
-              width={38}
-              iconSource={"options-outline"}
-              onPress={() => {
-                navigation.navigate("Filters");
-              }}
-            />
-          </View>
-          <View style={styles.filterTextContainer}>
-            <Pressable
-              onPress={() => {
-                setSelectedFilterText(0);
-                ApiCall(POPULAR_API_URL);
-              }}
-            >
-              <Text
-                style={
-                  selectedFilterText == 0
-                    ? styles.highlightedFilterText
-                    : styles.notHighlightedFilterText
-                }
-              >
-                Popular
-              </Text>
-            </Pressable>
-            <Pressable
-              onPress={() => {
-                setSelectedFilterText(1);
-                ApiCall(TOP_RATED_API_URL);
-              }}
-            >
-              <Text
-                style={
-                  selectedFilterText == 1
-                    ? styles.highlightedFilterText
-                    : styles.notHighlightedFilterText
-                }
-              >
-                Top Rated
-              </Text>
-            </Pressable>
-            <Pressable
-              onPress={() => {
-                setSelectedFilterText(2);
-                ApiCall(UPCOMING_API_URL);
-              }}
-            >
-              <Text
-                style={
-                  selectedFilterText == 2
-                    ? styles.highlightedFilterText
-                    : styles.notHighlightedFilterText
-                }
-              >
-                Upcoming
-              </Text>
-            </Pressable>
-          </View>
-          {movies && (
-            <View>
-              <FlatList
-                numColumns={windowDimensions.width<768 ? 2 : 3}
-                columnWrapperStyle={styles.movieList}
-                data={movies.results}
-                renderItem={({ item }) => (
-                  <MovieCard
-                    movieId={item.id}
-                    isBigCard={true}
-                    image={item.poster_path}
-                    title={item.title}
-                    key={item.id}
-                  />
-                )}
-              />
-            </View>
-          )}
+      <View style={styles.backgroundColor}>
+        <View style={styles.topContainer}>
+          <SearchFilter
+            searchMovies={searchMovies}
+            showIconLeft={true}
+            showIconRight={false}
+          />
+          <CustomButton
+            borderRadius={90}
+            defaultColor={"#E92348"}
+            pressedColor={"#373246"}
+            height={38}
+            width={38}
+            iconSource={"options-outline"}
+            onPress={() => {
+              navigation.navigate("Filters");
+            }}
+          />
         </View>
-      </ScrollView>
+        <View style={styles.filterTextContainer}>
+          <Pressable
+            onPress={() => {
+              setSelectedFilterText(0);
+              ApiCall(POPULAR_API_URL);
+            }}
+          >
+            <Text
+              style={
+                selectedFilterText == 0
+                  ? styles.highlightedFilterText
+                  : styles.notHighlightedFilterText
+              }
+            >
+              Popular
+            </Text>
+          </Pressable>
+          <Pressable
+            onPress={() => {
+              setSelectedFilterText(1);
+              ApiCall(TOP_RATED_API_URL);
+            }}
+          >
+            <Text
+              style={
+                selectedFilterText == 1
+                  ? styles.highlightedFilterText
+                  : styles.notHighlightedFilterText
+              }
+            >
+              Top Rated
+            </Text>
+          </Pressable>
+          <Pressable
+            onPress={() => {
+              setSelectedFilterText(2);
+              ApiCall(UPCOMING_API_URL);
+            }}
+          >
+            <Text
+              style={
+                selectedFilterText == 2
+                  ? styles.highlightedFilterText
+                  : styles.notHighlightedFilterText
+              }
+            >
+              Upcoming
+            </Text>
+          </Pressable>
+        </View>
+        {movies && (
+          <FlatList
+            numColumns={windowDimensions.width < 768 ? 2 : 3}
+            columnWrapperStyle={styles.movieList}
+            data={movies.results}
+            renderItem={({ item }) => (
+              <MovieCard
+                movieId={item.id}
+                isBigCard={true}
+                image={item.poster_path}
+                title={item.title}
+                key={item.id}
+              />
+            )}
+          />
+        )}
+      </View>
       {isLoading && <Loader />}
     </>
   );
@@ -149,6 +144,7 @@ export default function Search({ route }) {
 const styles = StyleSheet.create({
   backgroundColor: {
     backgroundColor: colors.mainColors.primary,
+    flex: 1,
   },
   topContainer: {
     flexDirection: "row",
@@ -162,19 +158,17 @@ const styles = StyleSheet.create({
   filterTextContainer: {
     flexDirection: "row",
     justifyContent: "center",
-    gap:"10%",
+    gap: 10,
   },
   highlightedFilterText: {
     fontSize: 18,
     fontFamily: "Ubuntu-Medium",
     color: "white",
-
   },
   notHighlightedFilterText: {
     fontSize: 18,
     fontFamily: "Ubuntu-Medium",
     color: colors.mainColors.gray1,
-
     opacity: 0.4,
   },
   movieList: {
